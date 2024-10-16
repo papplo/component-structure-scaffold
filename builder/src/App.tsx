d1173bd7b7d53a7c9daadd341c112588b65095d1
Pablo Anttila <pablo@queenslab.se>
2024-10-17 00:44:00 +0200

    Update app.tsx with new enchanced component.

--- builder/src/App.tsx
+++ builder/src/App.tsx
@@ -1,35 +1,73 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useDeferredValue, useState } from 'react'
 import './App.css'
+import { EnhancedCard as Card } from './components/enhanced'
+import { Button } from './components/stateless/Button'
+import { SvgImage } from './components/stateless/SvgImageShape'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  const deferredCount = useDeferredValue(count.toFixed(3).endsWith('000') ? count : count.toFixed(3))
+
+  function sum(n: number, increment = 1) {
+    return n + increment
+  }
+
+  function divide(n: number, divisor = 1) {
+    return n / divisor
+  }
+
+  function subtract(n: number, decrement = 1) {
+    return n - decrement
+  }
+
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <SvgImage width={100} height={100} fillColor={'#00FF00'} sides={Number(deferredCount)} ></SvgImage>
+      <h1>{deferredCount}</h1>
+      <section style={{ display: 'flex', justifyContent: 'space-between', maxWidth: '1280px', columnGap: '3em' }}>
+        <Card title='Sum'>
+          <Button label={'Sum 1'} onClick={() => setCount((count) => sum(count, 1))} />
+          <Button label={'Sum 3'} onClick={() => setCount((count) => sum(count, 3))} />
+          <Button label={'Sum 5'} onClick={() => setCount((count) => sum(count, 5))} />
+          <button onClick={(event) => {setCount(0); event.stopPropagation()}}>
+            Reset
+          </button>
+          <p>
+            In mathematics, the sum is an operation that combines two or more numbers to give a total value. It is a fundamental concept in arithmetic and is often used in various calculations.
+            To calculate the sum of two or more numbers, you simply add them together. For example, the sum of 2, 3, and 4 is 2 + 3 + 4 = 9.
+          </p>
+        </Card>
+      <Card title='Division'>
+        <Button label={'Divide by 2'} onClick={() => setCount((count) => divide(count, 2))} />
+        <Button label={'Divide by 3'} onClick={() => setCount((count) => divide(count, 3))} />
+        <Button label={'Divide by 5'} onClick={() => setCount((count) => divide(count, 5))} />
+        <button onClick={(event) => {setCount(0); event.stopPropagation()}}>
+          Reset
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          In mathematics, the sum is an operation that combines two or more numbers to give a total value. It is a fundamental concept in arithmetic and is often used in various calculations.
+          To calculate the sum of two or more numbers, you simply add them together. For example, the sum of 2, 3, and 4 is 2 + 3 + 4 = 9.
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </Card>
+      <Card title='Subtraction'>
+        <Button label={'Subtract 1'} onClick={() => setCount((count) => subtract(count, 1))} />
+        <Button label={'Subtract 3'} onClick={() => setCount((count) => subtract(count, 3))} />
+        <Button label={'Subtract 5'} onClick={() => setCount((count) => subtract(count, 5))} />
+        <button onClick={(event) => {setCount(0); event.stopPropagation()}}>
+          Reset
+        </button>
+        <p>
+          In mathematics, the sum is an operation that combines two or more numbers to give a total value. It is a fundamental concept in arithmetic and is often used in various calculations.
+          To calculate the sum of two or more numbers, you simply add them together. For example, the sum of 2, 3, and 4 is 2 + 3 + 4 = 9.
+        </p>
+      </Card>
+      </section>
+
     </>
   )
 }
 
 export default App
+
+
