770d59637fcc8705ef2e49649df493cd5a4cbd64
Pablo Anttila <pablo@queenslab.se>
2024-11-20 09:17:30 +0100

    wrap component in a new react scaffolded app

---
+++ packages/graphs/src/App.tsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+import DataPivotComponent from './components/DataPivotComponent'
+
+function App() {
+
+  return (
+      <DataPivotComponent endpoint="/your-api-endpoint" />
+
+  )
+}
+
+export default App
---
+++ packages/graphs/vitest.config.ts
@@ -0,0 +1,14 @@
+// vitest.config.ts
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
+    // Optionally, specify test file patterns
+    include: ['**/*.test.{ts,tsx}'],
+  },
+});
